f4fc8fb1685237ba27387d0819b6bc60af64c897
Geoffrey Spear <speargh@pitt.edu>
2012-07-05 10:22:24 -0400

    setup.py, gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pycounter',
+    version = '0.1',
+    packages = find_packages(),
+    author = 'Geoffrey Spear',
+    author_email = 'geoffspear@gmail.com',
+    keywords = 'library COUNTER journals usage_statistics',
+    test_suite = 'pycounter.test',
+    )
