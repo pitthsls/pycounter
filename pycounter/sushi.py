8bb1b7c4369a8bbf62ae47d4b88e5b3174c6bd6a
Geoffrey Spear <speargh@pitt.edu>
2014-11-04 13:56:22 -0500

    add basic SUSHI fetch support

---
+++ pycounter/sushi.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+"""NISO SUSHI support"""
+
+from suds.client import Client
+
+
+def get_sushi_stats_raw(wsdlurl, start_date, end_date, requestor_id=None,
+                        requestor_email=None, customer_reference=None,
+                        report="JR1", release=4):
+    """Get SUSHI stats for a given site"""
+    client = Client(wsdlurl)
+    rdef = client.factory.create('ns1:ReportDefinition')
+
+    rdef._Name = report
+    rdef._Release = release
+    rdef.Filters.UsageDateRange.Begin = start_date
+    rdef.Filters.UsageDateRange.End = end_date
+
+    cref = client.factory.create('ns1:CustomerReference')
+    cref.ID = customer_reference
+
+    reqr = client.factory.create('ns1:Requestor')
+    reqr.ID = requestor_id
+    if requestor_email is not None:
+        reqr.Email = requestor_email
+
+    report = client.service.GetReport(reqr, cref, rdef)
+
+    return report
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pycounter',
-    version = '0.2',
+    version = '0.3',
     packages = find_packages(),
     author = 'Geoffrey Spear',
     author_email = 'geoffspear@gmail.com',
     keywords = 'library COUNTER journals usage_statistics',
     test_suite = 'pycounter.test',
-    install_requires = ['pyisbn', 'openpyxl']
+    install_requires = ['pyisbn', 'openpyxl', 'suds']
     )
