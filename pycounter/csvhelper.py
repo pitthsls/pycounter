372049a5f0fe37c6c733751e56243c40e120bf22
Geoffrey Spear <speargh@pitt.edu>
2014-11-10 09:57:32 -0500

    Begin py3k support

---
+++ pycounter/csvhelper.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import
+
+"""Read CSV as unicode from both python 2 and 3 transparently"""
+
+import sys, csv, codecs
+
+import six
+
+class UnicodeReader(six.Iterator):
+    def __init__(self, filename, dialect=csv.excel,
+                 encoding="utf-8", **kw):
+        self.filename = filename
+        self.dialect = dialect
+        self.encoding = encoding
+        self.kw = kw
+
+    def __enter__(self):
+        if six.PY3:
+            self.f = open(self.filename, 'rt',
+                          encoding=self.encoding, newline='')
+        else:
+            self.f = open(self.filename, 'rb')
+        self.reader = csv.reader(self.f, dialect=self.dialect,
+                                 **self.kw)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.f.close()
+
+    def __next__(self):
+        row = next(self.reader)
+        if six.PY3:
+            return row
+        return [s.decode("utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+class UnicodeWriter(object):
+    def __init__(self, filename, dialect=csv.excel,
+                 encoding="utf-8", **kw):
+        self.filename = filename
+        self.dialect = dialect
+        self.encoding = encoding
+        self.kw = kw
+
+    def __enter__(self):
+        if six.PY3:
+            self.f = open(self.filename, 'wt',
+                          encoding=self.encoding, newline='')
+        else:
+            self.f = open(self.filename, 'wb')
+        self.writer = csv.writer(self.f, dialect=self.dialect,
+                                 **self.kw)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.f.close()
+
+    def writerow(self, row):
+        if not six.PY3:
+            row = [s.encode(self.encoding) for s in row]
+        self.writer.writerow(row)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email = 'geoffspear@gmail.com',
     keywords = 'library COUNTER journals usage_statistics',
     test_suite = 'pycounter.test',
-    install_requires = ['pyisbn', 'openpyxl', 'suds']
+    install_requires = ['pyisbn', 'openpyxl', 'suds', 'six']
     )
