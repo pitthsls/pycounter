48fb8a4ec51a0596eb0a566656e2fd2fc9682485
Geoffrey Spear <speargh@pitt.edu>
2014-11-12 11:02:04 -0500

    further DRY; parse all files with the same function that takes iterators of COUNTER rows.

--- pycounter/report.py
+++ pycounter/report.py
@@ -94,46 +94,66 @@ def parse(filename):
 
 
 def parse_xlsx(filename):
-    """Open COUNTER XLSX report with given filename and parse into a
-    CounterReport object"""
     from openpyxl import load_workbook
-    workbook = load_workbook(filename)
+    workbook = load_workbook(filename = filename)
     worksheet = workbook.get_sheet_by_name(workbook.get_sheet_names()[0])
+    row_it = worksheet.iter_rows()
+    split_row_list = ([cell.value if cell.value is not None else ""
+                       for cell in row] for row in row_it)
 
+    return parse_generic(split_row_list)
+
+
+def parse_separated(filename, delimiter):
+    """Open COUNTER CSV/TSV report with given filename and delimiter
+    and parse into a CounterReport object"""
+    with csvhelper.UnicodeReader(filename,
+                                 delimiter=delimiter) as report_reader:
+        return parse_generic(report_reader)
+
+
+def parse_generic(report_reader):
+    """Takes an iterator of COUNTER report rows and
+    returns a CounterReport object"""
     report = CounterReport()
 
-    line1 = worksheet['A1'].value
+    line1 = six.next(report_reader)
 
-    rt_match = re.match(r'.*(Journal|Book|Database) Report (\d) ?\(R(\d)\)',
-                        line1)
+    rt_match = re.match(
+        r'.*(Journal|Book|Database) Report (\d) ?\(R(\d)\)',
+        line1[0])
     if rt_match:
         report.report_type = (rt_match.group(1)[0].capitalize() + 'R' +
                               rt_match.group(2))
         report.report_version = int(rt_match.group(3))
 
-    # skip 3 lines we don't care about
-    curr_row = 4
+    for _ in range(3):
+        six.next(report_reader)
     if report.report_version == 4:
         # COUNTER 4 has 3 more lines of introduction
-        curr_row = 7
-
-    header = [x.value for x in worksheet.rows[curr_row]]
+        for _ in range(3):
+            six.next(report_reader)
+    header = six.next(report_reader)
     first_date_col = 10 if report.report_version == 4 else 5
     if report.report_type in ('BR1', 'BR2') and report.report_version == 4:
         first_date_col = 8
+
     report.year = int(header[first_date_col].split('-')[1])
     if report.year < 100:
         report.year += 2000
 
     if report.report_version == 4:
-        last_col = len(header) - 1
+        countable_header = header[0:8]
+        for col in header[8:]:
+            if col:
+                countable_header.append(col)
+        last_col = len(countable_header)
     else:
         for last_col, v in enumerate(header):
             if 'YTD' in v:
                 break
-    curr_row += 2
-    for xlsrow in worksheet.rows[curr_row:]:
-        line = [x.value if x.value is not None else "" for x in xlsrow]
+    six.next(report_reader)
+    for line in report_reader:
         if not line:
             continue
         if report.report_version == 4:
@@ -155,72 +175,14 @@ def parse_xlsx(filename):
     return report
 
 
-def parse_separated(filename, delimiter):
-    """Open COUNTER CSV/TSV report with given filename and delimiter
-    and parse into a CounterReport object"""
-    with csvhelper.UnicodeReader(filename,
-                                 delimiter=delimiter) as report_reader:
-        report = CounterReport()
-
-        line1 = six.next(report_reader)
-
-        rt_match = re.match(
-            r'.*(Journal|Book|Database) Report (\d) ?\(R(\d)\)',
-            line1[0])
-        if rt_match:
-            report.report_type = (rt_match.group(1)[0].capitalize() + 'R' +
-                                  rt_match.group(2))
-            report.report_version = int(rt_match.group(3))
-
-        for _ in range(3):
-            six.next(report_reader)
-        if report.report_version == 4:
-            # COUNTER 4 has 3 more lines of introduction
-            for _ in range(3):
-                six.next(report_reader)
-        header = six.next(report_reader)
-        first_date_col = 10 if report.report_version == 4 else 5
-        if report.report_type in ('BR1', 'BR2') and report.report_version == 4:
-            first_date_col = 8
-
-        report.year = int(header[first_date_col].split('-')[1])
-        if report.year < 100:
-            report.year += 2000
-
-        if report.report_version == 4:
-            last_col = len(header)
-        else:
-            for last_col, v in enumerate(header):
-                if 'YTD' in v:
-                    break
-        six.next(report_reader)
-        for line in report_reader:
-            if not line:
-                continue
-            if report.report_version == 4:
-                if report.report_type == 'JR1':
-                    line = line[0:3] + line[5:7] + line[10:last_col]
-                elif report.report_type in ('BR1', 'BR2'):
-                    line = line[0:3] + line[5:7] + line[8:last_col]
-            else:
-                line = line[0:last_col]
-            logging.debug(line)
-            if report.report_type:
-                if report.report_type.startswith('JR'):
-                    report.pubs.append(CounterPublication(line))
-                elif report.report_type.startswith('BR'):
-                    report.pubs.append(CounterBook(line))
-                else:
-                    raise UnknownReportTypeError(report.report_type)
-
-        return report
-
 
 def parse_csv(filename):
     warnings.warn(".parse_csv is deprecated; use parse_separated",
                   DeprecationWarning)
+    return parse_separated(filename, ",")
 
 
 def parse_tsv(filename):
     warnings.warn(".parse_tsv is deprecated; use parse_separated",
                   DeprecationWarning)
+    return parse_separated(filename, "\t")
