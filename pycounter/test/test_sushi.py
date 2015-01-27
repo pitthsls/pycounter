b0ff5cd2efb187574931be2a910d700799a30c2f
Geoffrey Spear <speargh@pitt.edu>
2015-01-27 10:18:39 -0500

    simple SUSHI XML; test report parameters

--- pycounter/test/test_sushi.py
+++ pycounter/test/test_sushi.py
@@ -7,8 +7,21 @@ import os
 
 from pycounter import sushi
 
+
 class TestHelpers(unittest.TestCase):
     def test_ns(self):
         self.assertEqual(
             sushi._ns("sushi", "name"),
             "{http://www.niso.org/schemas/sushi}name")
+
+
+class TestConvertRawSimple(unittest.TestCase):
+    def setUp(self):
+        path = os.path.join(os.path.dirname(__file__),
+                            'data', 'sushi_simple.xml')
+        with open(path, 'rb') as datafile:
+            self.report = sushi._raw_to_full(datafile.read())
+
+    def test_report(self):
+        self.assertEqual(self.report.report_type, u'JR1')
+        self.assertEqual(self.report.report_version, 4)
