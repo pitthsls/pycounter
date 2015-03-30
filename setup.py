8d7d0a6eea39be02a8d579d83b75341c5ad31579
Geoffrey Spear <geoffspear@gmail.com>
2015-03-30 12:54:54 -0400

    bump version and add readme to pypi

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ version = {}  # will be set by exec below
 with open('pycounter/version.py', 'rb') as fp:
     exec(fp.read(), version)
 
+with open('README.rst') as readmefile:
+    readme = readmefile.read()
+
 setup(
     name='pycounter',
     version=version['__version__'],
@@ -15,6 +18,7 @@ setup(
     maintainer_email='speargh@pitt.edu',
     url='http://www.github.com/pitthsls/pycounter',
     description='Project COUNTER/NISO SUSHI statistics',
+    long_description=readme,
     keywords='library COUNTER journals usage_statistics SUSHI',
     test_suite='pycounter.test',
     classifiers=[
