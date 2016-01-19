2ee272934a12ec369d8ae6461a3d8c2a92590c4b
Geoffrey Spear <geoffspear@gmail.com>
2016-01-19 14:18:55 -0500

    coverage: test file with multiple publishers and platforms (should bring us to 100% coverage)

--- pycounter/test/test_output_jr1.py
+++ pycounter/test/test_output_jr1.py
@@ -55,3 +55,28 @@ class TestWritingJR1(unittest.TestCase):
             new_content = new_file.read()
 
         self.assertEqual(orig_content, new_content)
+
+
+class TestOutputJR1Multi(unittest.TestCase):
+    """Test output of JR1 - Multiple publishers and platforms."""
+    def setUp(self):
+        filename = os.path.join(os.path.dirname(__file__), 'data/C4JR1mul.csv')
+        rep = report.parse(filename)
+        with csvhelper.UnicodeReader(filename,
+                                     delimiter=',') as report_reader:
+            self.file_content = list(report_reader)
+
+        self.output_content = rep.as_generic()
+
+    def test_header_content(self):
+        self.assertEqual(self.file_content[0:7], self.output_content[0:7])
+
+    def test_table_header(self):
+        self.assertEqual(self.file_content[7], self.output_content[7])
+
+    def test_totals(self):
+        self.assertEqual(self.file_content, self.output_content)
+
+    def test_data(self):
+        for index, line in enumerate(self.file_content[9:], 9):
+            self.assertEqual(line, self.output_content[index])
