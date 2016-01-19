1c2eebe236dcfcc607749ebcba7a769bb27b5176
Geoffrey Spear <geoffspear@gmail.com>
2016-01-19 11:18:35 -0500

    coverage: test creation of blank CounterJournal item

---
+++ pycounter/test/test_classes.py
@@ -0,0 +1,9 @@
+import unittest
+
+from pycounter import report
+
+
+class TestJournalClass(unittest.TestCase):
+    def test_counter_journal(self):
+        journal = report.CounterJournal()
+        self.assertEqual(journal.issn, "")
