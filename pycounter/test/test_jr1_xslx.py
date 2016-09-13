ce4db008413c4ce0aac3cd6b801694cc8f2791c5
Geoffrey Spear <speargh@pitt.edu>
2016-09-13 09:55:47 -0400

    remove a looped assert, and convert last use of unittest in this file

--- pycounter/test/test_jr1_xslx.py
+++ pycounter/test/test_jr1_xslx.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import
 
 import os
-import unittest
 
 import pytest
 
@@ -29,51 +28,21 @@ def test_year(jr1_report):
 
 
 def test_publisher(jr1_report):
-    for publication in jr1_report:
-        assert publication.publisher == u'American Medical Association'
+    assert all(publication.publisher == u'American Medical Association'
+               for publication in jr1_report)
 
 
 def test_platform(jr1_report):
-    for publication in jr1_report:
-        assert publication.platform == u'Silverchair'
+    assert all(publication.platform == u'Silverchair'
+               for publication in jr1_report)
 
 
-class ParseExample(unittest.TestCase):
-    """Test XLSX JR 1"""
-
-    def setUp(self):
-        self.report = report.parse(os.path.join(os.path.dirname(__file__),
-                                                'data/JR1.xlsx'))
-
-    def test_stats(self):
-        publication = self.report.pubs[0]
-        self.assertEqual([x[2] for x in publication],
-                         [4, 14, 13, 30, 19, 7, 31, 6, 16, 12, 8, 12])
-        publication = self.report.pubs[1]
-        self.assertEqual(
-            [x[2] for x in publication],
-            [5414, 5459, 4936, 5172, 4064, 3904, 4054,
-             4090, 5010, 6680, 5961, 3742])
-
-
-class ParseBadExample(unittest.TestCase):
-    """Test XLSX JR 1 with Excel-isms instead of COUNTER strings"""
-
-    def setUp(self):
-        self.report = report.parse(os.path.join(os.path.dirname(__file__),
-                                                'data/JR1_bad.xlsx'))
-
-    def test_stats(self):
-        publication = self.report.pubs[0]
-        self.assertEqual([x[2] for x in publication],
-                         [4, 14, 13, 30, 19, 7, 31, 6, 16, 12, 8, 12])
-        publication = self.report.pubs[1]
-        self.assertEqual(
-            [x[2] for x in publication],
-            [5414, 5459, 4936, 5172, 4064, 3904, 4054,
-             4090, 5010, 6680, 5961, 3742])
-        publication = self.report.pubs[8]
-        self.assertEqual(
-            [x[2] for x in publication],
-            [592, 574, 502, 616,
-             349, 476, 460, 383, 434, 496, 522, 304])
+@pytest.mark.parametrize('pub_number,expected', [
+    (0, [4, 14, 13, 30, 19, 7, 31, 6, 16, 12, 8, 12]),
+    (1, [5414, 5459, 4936, 5172, 4064, 3904, 4054, 4090, 5010, 6680, 5961,
+         3742]),
+    (8, [592, 574, 502, 616, 349, 476, 460, 383, 434, 496, 522, 304]),
+])
+def test_stats(jr1_report, pub_number, expected):
+    publication = jr1_report.pubs[pub_number]
+    assert [x[2] for x in publication] == expected
