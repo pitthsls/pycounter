67f77d07c60193acec169d0c7e2a2a75cc9b4bab
Geoffrey Spear <geoffspear@gmail.com>
2018-04-25 12:41:34 -0400

    add test for gaps in stats being output correctly

--- pycounter/test/test_classes.py
+++ pycounter/test/test_classes.py
@@ -1,8 +1,22 @@
 """Tests regarding instantiating classes"""
 
 from pycounter import report
+import datetime
 
 
 def test_counter_journal():
     journal = report.CounterJournal()
     assert journal.issn == u""
+
+
+def test_report_gaps():
+    journal = report.CounterJournal(
+        period=(datetime.date(2018, 1, 1), datetime.date(2018, 3, 1)),
+        month_data=[(datetime.date(2018, 1, 1), 50),
+                    (datetime.date(2018, 3, 1), 99),
+                    ]
+    )
+
+    output_data = journal.as_generic()
+    assert output_data[11] == '0'
+    assert output_data[12] == '99'
