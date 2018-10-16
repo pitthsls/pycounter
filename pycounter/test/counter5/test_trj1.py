c2f1340b7bb061fc5dad511c4839f5e3ccaff30e
Geoffrey Spear <geoffspear@gmail.com>
2018-10-16 14:58:02 -0400

    style: blacken and refactor a long conditional repeated

--- pycounter/test/counter5/test_trj1.py
+++ pycounter/test/counter5/test_trj1.py
@@ -26,5 +26,7 @@ def test_period(trj1_report):
 
 
 def test_parse():
-    report = pycounter.report.parse(os.path.join(os.path.dirname(__file__), "data", "tr_j1.tsv"))
+    report = pycounter.report.parse(
+        os.path.join(os.path.dirname(__file__), "data", "tr_j1.tsv")
+    )
     assert report.report_version == 5
