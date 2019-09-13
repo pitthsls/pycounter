0094f42d376ea3917160fcec961388f42a14764b
Geoffrey Spear <geoffspear@gmail.com>
2019-09-13 15:45:42 -0400

    pyupgrade rewrite

--- pycounter/test/test_output_jr1.py
+++ pycounter/test/test_output_jr1.py
@@ -11,29 +11,29 @@ def test_ordering_output():
     report = pycounter.report.CounterReport(
         report_type="JR1",
         report_version=4,
-        customer=u"Foo",
-        institutional_identifier=u"Bar",
+        customer="Foo",
+        institutional_identifier="Bar",
         period=(datetime.date(2016, 1, 1), datetime.date(2016, 1, 31)),
     )
     report.pubs.append(
         pycounter.report.CounterJournal(
             period=(datetime.date(2016, 1, 1), datetime.date(2016, 1, 31)),
             month_data=[(datetime.date(2016, 1, 1), 260)],
-            title=u"Fake Journal",
-            publisher=u"No one",
-            platform=u"Your imagination",
+            title="Fake Journal",
+            publisher="No one",
+            platform="Your imagination",
         )
     )
     report.pubs.append(
         pycounter.report.CounterJournal(
             period=(datetime.date(2016, 1, 1), datetime.date(2016, 1, 31)),
             month_data=[(datetime.date(2016, 1, 1), 62)],
-            title=u"Another Fake Journal",
-            publisher=u"No one",
-            platform=u"Your imagination",
+            title="Another Fake Journal",
+            publisher="No one",
+            platform="Your imagination",
         )
     )
     output_content = report.as_generic()
 
-    assert output_content[9][0] == u"Another Fake Journal"
-    assert output_content[10][0] == u"Fake Journal"
+    assert output_content[9][0] == "Another Fake Journal"
+    assert output_content[10][0] == "Fake Journal"
