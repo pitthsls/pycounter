f49b1bec361b398c2cbb11f47c73ea0a6e41467f
Geoffrey Spear <geoffspear@gmail.com>
2019-09-13 12:20:14 -0400

    check-python-versions support

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     extras_require={
         "docs": ["sphinx", "sphinx_rtd_theme"],
