99660a2cb97e9ad82b91b650e50732eb4289d7c6
Wooble <speargh@pitt.edu>
2025-10-27 14:45:01 -0400

    lint: remove u strings and don't warn for possible f strings

--- pycounter/test/counter5/test_trj2.py
+++ pycounter/test/counter5/test_trj2.py
@@ -8,11 +8,11 @@ def test_metric(trj2_report):
 
 
 def test_type(trj2_report):
-    assert trj2_report.report_type == u"TR_J2"
+    assert trj2_report.report_type == "TR_J2"
 
 
 def test_data(trj2_report):
     i = iter(trj2_report)
     row = next(i)
     item = next(iter(row))
-    assert item == (datetime.date(2017, 1, 1), u"Limit_Exceeded", 3)
+    assert item == (datetime.date(2017, 1, 1), "Limit_Exceeded", 3)
